e3d344b9ed1b3888c703a5f3abaa7dc7f59371dc
bhuvana-ship <incerd.bhuvana@gmail.com>
2019-08-27 14:36:15 +0530

    Bhuvana

---
+++ js/all.js
@@ -0,0 +1,17 @@
+// var x = document.getElementById("demo");
+// var example = x.getContext("2d");
+// example.moveTo(0, 0);
+// example.lineTo(300, 200);
+// example.stroke();
+
+// var canvasDemo = document.getElementById("demo");
+// var fillCanvas = canvasDemo.getContext("2d");
+// fillCanvas.moveTo(0, 0);
+// fillCanvas.lineTo(200, 100);
+// fillCanvas.stroke();
+
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// ctx.moveTo(0,0);
+// ctx.lineTo(200,100);
+// ctx.stroke();
